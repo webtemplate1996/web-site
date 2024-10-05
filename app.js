46177e511ded2956c4996a40a1c7477f26ee38ef
webtemplate19996 <sosachango1@gmail.com>
2024-10-04 20:20:52 -0500

    primer commit

---
+++ app.js
@@ -0,0 +1,29 @@
+window.onload = function(){
+    document.getElementById('box').classList.add('visible')
+} 
+
+// Las palabras que quieres alternar
+const words = ["Landing Page", "Website","Web Responsive"];
+let index = 0;
+
+// Selecciona el span donde se cambiarán las palabras
+const wordElement = document.querySelector('.word');
+
+// Función para cambiar palabras
+function changeWord() {
+    wordElement.classList.remove('active'); // Hace invisible la palabra actual
+
+    setTimeout(() => {
+        index = (index + 1) % words.length; // Avanza al siguiente índice de palabra
+        wordElement.textContent = words[index]; // Cambia el texto de la palabra
+        wordElement.classList.add('active'); // Hace visible la nueva palabra
+    }, 1000); // Tiempo en milisegundos para la transición (1 segundo)
+}
+
+// Cambia la palabra cada 3 segundos
+setInterval(changeWord, 5000);
+
+
+document.getElementById('year').textContent = new Date().getFullYear();
+
+ 
